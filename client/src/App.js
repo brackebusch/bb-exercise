fa8eb7232f4e3d03dcf140c05c092995bcfcc14f
brackebusch <Kyle Brackebusch>
2018-10-07 18:16:33 -0700

    table added

--- client/src/App.js
+++ client/src/App.js
@@ -17,21 +17,34 @@ class App extends Component {
     return (
       <div className='App'>
         <div className='Body'>
+          <img src={logo} className='App-logo' alt='logo' />
 
-        <img src={logo} className='App-logo' alt='logo' />
-        <div className='Header'>
-        <div className='Date-Title'>
-          <div className='Date'>
-            <span className='Month'>{moment().format('MMM')}</span>
-            <span className='Day'>{moment().format('DD')}</span>
+          <div className='Header'>
+            <div className='Date-Title'>
+              <div className='Date'>
+                <span className='Month'>{moment().format('MMM')}</span>
+                <span className='Day'>{moment().format('DD')}</span>
+              </div>
+              <span className='Title'>Perfectly Ground Work Work Orders</span>
+            </div>
+            <button>CREATE ORDER</button>
           </div>
-          <span className='Title'>Perfectly Ground Work Work Orders</span>
-        </div>
-          <button>
-            CREATE ORDER
-          </button>
-        </div>
-          
+
+          <div className='Table'>
+            <h4>ORDERS</h4>
+            <hr/>
+            <table>
+              <tr>
+                <th>Coffee</th>
+                <th>Method</th>
+                <th>Number of Cases</th>
+                <th>Packets per Case</th>
+                <th>Ship Date</th>
+                <th>Order</th>
+                <th>View</th>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     );
