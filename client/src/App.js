153d110ccf5cb35a938558e53d45775048947ee1
brackebusch <Kyle Brackebusch>
2018-10-08 01:13:42 -0700

    setup modal

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import moment from 'moment';
-// import { Table } from 'evergreen-ui'
-// import { TableHead, TextTableHeaderCell, TextTableCell, TableRow } from 'evergreen-ui/commonjs/table';
+import { Dialog, Combobox } from 'evergreen-ui'
 
 class App extends Component {
-  state = {data: false, index: 0}
+  state = {data: false, index: 0, showModal: false}
 
   async componentDidMount(){
     const response = await fetch('/work_orders')
@@ -19,11 +18,10 @@ class App extends Component {
       return <tr><td>Loading...</td></tr>
     } 
     const rows = []
-    console.log(this.state.data);
     for(let i = this.state.index; i < (this.state.index + 25); i++){
       rows.push(
         <tr key={this.state.data[i].id} className={i % 2 === 0 ? '' : 'Striped'}>
-          <td width="22%">{this.state.data[i].coffee}</td>
+          <td width='22%'>{this.state.data[i].coffee}</td>
           <td>{this.state.data[i].method}</td>
           <td>{this.state.data[i].cases}</td>
           <td>{this.state.data[i].packets}</td>
@@ -31,17 +29,37 @@ class App extends Component {
             {moment(this.state.data[i].ship_date).format('MM/DD/YYYY')}
             {this.state.data[i].priority ? ' ★' : ''}
           </td>
-          <td>#{this.state.data[i].id}</td>
-          <td>View</td>
+          <td className='Order'>#{this.state.data[i].id}</td>
+          <td><i class='fas fa-eye'></i></td>
         </tr>
       )
     }
     return rows
   }
 
+  modalState() {
+    console.log(this.state.data);
+    // console.log(this.state)
+    // this.setState({show: !this.state.showModal})
+  }
+
   render() {
     return (
       <div className='App'>
+        <Dialog
+          isShown={this.state.show}
+          title='Perfectly Ground Work Orders'
+          confirmLabel='Custom Label'
+          >
+        Instructional text would go here - Lorem ipsum dolo sit amet, consecteur adipiscing elit. Nallam feugiat libero eget diam.
+        <Combobox
+          defaultSelectedItem={{ label: 'Giant Steps' }}
+          items={[{ label: 'Bella Donovan' }, { label: 'Giant Steps' }]}
+          itemToString={item => item.label}
+          onChange={selected => console.log(selected)}
+        />
+        <button onClick={() => this.modalState}>SUBMIT WORK ORDER</button>
+        </Dialog>
         <div className='Body'>
           <img src={logo} className='App-logo' alt='logo' />
 
@@ -51,9 +69,9 @@ class App extends Component {
                 <span className='Month'>{moment().format('MMM')}</span>
                 <span className='Day'>{moment().format('DD')}</span>
               </div>
-              <span className='Title'>Perfectly Ground Work Work Orders</span>
+              <span className='Title'>Perfectly Ground Work Orders</span>
             </div>
-            <button>CREATE ORDER</button>
+            <button onClick={() => this.modalState}>CREATE ORDER</button>
           </div>
           <div className='Table'>
             <h4>ORDERS</h4>
@@ -65,7 +83,7 @@ class App extends Component {
                   <th><br/>Method</th>
                   <th>Number of Cases</th>
                   <th>Packets per Case</th>
-                  <th><br/>Ship Date</th>
+                  <th><br/>Ship Date <i class='fas fa-sort-amount-down'></i></th>
                   <th><br/>Order</th>
                   <th><br/>View</th>
                 </tr>
